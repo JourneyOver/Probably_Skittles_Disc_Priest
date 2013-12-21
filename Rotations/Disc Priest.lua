33692fd321fe34f420b71c95458de366ab3f653d
SourSkittles <timtag1190@hotmail.com>
2013-12-21 12:29:22 -0600

    Create Disc Priest.lua

---
+++ Rotations/Disc Priest.lua
@@ -0,0 +1,16 @@
+-- ProbablyEngine Rotation Packager
+-- Custom Discipline Priest Rotation
+-- Created on Dec 21st 2013 5:57 pm
+
+
+  -- =======================================
+  --          Your Rotation Here!
+  -- =======================================
+  -- Everything between the first set of { }
+  -- in the core rotations can go here
+  -- =======================================
+  -- If you have any questions, please ask
+  -- on the ProbablyEngine Forums!
+  -- =======================================
+
+})
